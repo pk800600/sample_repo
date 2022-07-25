9df6bd75d438009a27c4c50e4246b92f7b9f9c4a
pk800600 <pankaj800600@gmail.com>
2022-07-25 11:43:04 -0400

    First commit

---
+++ src/test/java/com/maven/git/practice/GitBasePractice.java
@@ -0,0 +1,11 @@
+package com.maven.git.practice;
+
+public class GitBasePractice {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Git prectice");
+		System.out.println("git prectice2");
+	}
+
+}
